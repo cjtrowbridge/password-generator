291ce80549752dc7ff3d682f65db5bea8671f98f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-29 11:38:37 -0800

    Update index.php

--- index.php
+++ index.php
@@ -47,32 +47,11 @@ $Sizes=array(
 
   <nav class="navbar navbar-fixed-top navbar-dark bg-primary container">
     <a class="navbar-brand" href="https://password-generator.cjtrowbridge.com">Password Generator</a>
-    
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="https://github.com/cjtrowbridge/password-generator">Github</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown link
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-      </ul>
-    </div>
-    
-    
+    <ul class="nav navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="https://github.com/cjtrowbridge/password-generator">Github</a>
+      </li>
+    </ul>
   </nav>
 
   <div class="container" id="bodyContainer">
